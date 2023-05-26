fee3978f490a9446587c5cea1ca185d57d0c19af
Pratyush <77561223+Pratyushk2003@users.noreply.github.com>
2023-05-26 12:15:52 +0530

    Update script.js

--- hospital/script.js
+++ hospital/script.js
@@ -2,11 +2,38 @@ document.getElementById('appointmentForm').addEventListener('submit', function(e
   event.preventDefault();
   var name = document.getElementById('name').value;
   var email = document.getElementById('email').value;
-  var message = document.getElementById('message').value;
-  
-  // Send the data to the server (backend)
-  var data = {
-    name: name,
-    email: email,
-    message: message
-  };
+  const userInput = document.getElementById('user-input');
+  form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const userMessage = userInput.value;
+        if (userMessage.trim() === '') {
+          return;
+        }
+
+        userInput.value = '';
+        appendMessage(userMessage, 'user');
+
+        try {
+          const response = await fetch('/generate', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ text: userMessage })
+          });
+
+          const data = await response.json();
+          const reply = data.reply;
+          appendMessage(reply, 'bot');
+        } catch (error) {
+          console.error(error);
+          appendMessage('Something went wrong', 'bot');
+        }
+      });
+  function appendMessage(message, sender) {
+        const messageElement = document.createElement('div');
+        messageElement.classList.add(sender === 'user' ? 'user-message' : 'bot-message');
+        messageElement.textContent = message;
+        chatLog.appendChild(messageElement);
+      }
